58a5ac7fb846c31f23b61113774f308b1cd39985
Dom Le <lequydung101@yahoo.com>
2021-03-21 16:14:23 +1100

    Updates

---
+++ precache-manifest.b12eb803f54048c57fae329d79ebc58c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be85da0fe6357ea0b0d914f26830d53",
+    "url": "/portfolio_2.0/index.html"
+  },
+  {
+    "revision": "e65e4e8e9919e83a4cc1",
+    "url": "/portfolio_2.0/static/css/main.acb6634a.chunk.css"
+  },
+  {
+    "revision": "753eabc6bc873d7a482c",
+    "url": "/portfolio_2.0/static/js/2.f91f5b2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio_2.0/static/js/2.f91f5b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e65e4e8e9919e83a4cc1",
+    "url": "/portfolio_2.0/static/js/main.3d9dce02.chunk.js"
+  },
+  {
+    "revision": "b8a570d6b56932a15468",
+    "url": "/portfolio_2.0/static/js/runtime-main.e433744d.js"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/portfolio_2.0/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/portfolio_2.0/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/portfolio_2.0/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/portfolio_2.0/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/portfolio_2.0/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/portfolio_2.0/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/portfolio_2.0/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/portfolio_2.0/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "cb18e4d206aca956ee53d510a20ec222",
+    "url": "/portfolio_2.0/static/media/univer.cb18e4d2.jpg"
+  }
+]);
